29042566ae4b5d25aa039b0599706ddb5ddb3b57
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-01-27 12:35:31 +0100

    Created a renderer

---
+++ src/main/java/Renderer/Renderer.java
@@ -0,0 +1,27 @@
+package Renderer;
+
+enum API {
+    OPENGL,
+    OPENGL_ES;
+}
+
+public class Renderer {
+
+    API m_Api;
+    public void Init(API _api) {
+        
+    }
+    public void Draw() {
+
+    }
+    public void ClearColor() {
+
+    }
+    public void ClearRender() {
+
+    }
+    public void onResize() {
+
+    }
+
+}
