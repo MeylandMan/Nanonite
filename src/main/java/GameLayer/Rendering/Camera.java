2cd83e331c39ae14c2a8a4346cc937d35b217285
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-03 16:56:00 +0100

    Added ProcessKeyboard method

--- src/main/java/GameLayer/Rendering/Camera.java
+++ src/main/java/GameLayer/Rendering/Camera.java
@@ -2,6 +2,8 @@ package GameLayer.Rendering;
 
 import org.joml.*;
 
+import java.util.ArrayList;
+
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
     FORWARD,
@@ -52,5 +54,31 @@ public class Camera {
         return mat4.lookAt(Position, temp, Up);
     }
 
-    
+    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    {
+        float velocity = MovementSpeed * deltaTime;
+        Vector3fc[] temp = {
+                new Vector3f(),
+                new Vector3f()
+        };
+
+        temp[0].mul(velocity,Front);
+        temp[1].mul(velocity,Right);
+        switch(direction) {
+            case Camera_Movement.FORWARD:
+                Position = temp[0].add(temp[0], Position);
+                break;
+            case Camera_Movement.BACKWARD:
+                Position = temp[0].sub(temp[0], Position);
+                break;
+            case Camera_Movement.LEFT:
+                Position = temp[1].sub(temp[1], Position);
+                break;
+            case Camera_Movement.RIGHT:
+                Position = temp[1].add(temp[1], Position);
+                break;
+        }
+
+    }
 }
